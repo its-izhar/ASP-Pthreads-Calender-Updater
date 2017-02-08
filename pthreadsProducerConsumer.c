e5e285b40f80c094da7f3eac6ef6d7fb31f94213
Izhar Shaikh <izharits@gmail.com>
2017-02-08 03:01:10 -0500

    Added logic to terminate the consumer thread after producer has finished the job and consumer has consumed it.

--- pthreadsProducerConsumer.c
+++ pthreadsProducerConsumer.c
@@ -4,7 +4,7 @@
  * @Email:  izharits@gmail.com
  * @Filename: pthreadsExample.c
 * @Last modified by:   Izhar Shaikh
-* @Last modified time: 2017-02-07T22:54:10-05:00
+* @Last modified time: 2017-02-08T02:56:40-05:00
  */
 
 #include <stdio.h>
@@ -14,6 +14,7 @@
 #include <time.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 #include "debugMacros.h"
 #include "pthreadsProducerConsumer.h"
 
@@ -25,46 +26,57 @@ pthread_cond_t full;
 pthread_cond_t empty;
 
 int numItems = 0;
+bool producerDone = false;
 
 #define MAX_STRINGS 6
 char *array[MAX_STRINGS] = {"Hello", "Hi","Goodbye!", "Coffee", "Tea", "House"};
 
 
+/* Producer */
 static void *producerThread(void *arg)
 {
   // Get the handle for the data
   circularBuffer_t *buffer = (circularBuffer_t *) arg;
+  int i=5000;
 
-  while(1)
+  while(i--)
   {
-    sleep(rand() % SLEEP_LIMIT);
     // Get the item
     int itemIndex = rand() % MAX_STRINGS;
 
-    // Critical Section
+    // Enter: Critical Section
     pthread_mutex_lock(&lock);
+      dbg_info("PRODUCER: Acquired Lock!\n");
+      while(numItems == buffer->capacity){
+        pthread_cond_wait(&full, &lock);
+      }
+      dbg_trace("PRODUCER: Writing to buffer...%s\n", array[itemIndex]);
+      memset(buffer->items[buffer->in], 0, BUFFERSIZE*sizeof(char));
+      strncpy(buffer->items[buffer->in], array[itemIndex], BUFFERSIZE);
+      buffer->in = (buffer->in + 1) % buffer->capacity;
+      ++numItems;
+      dbg_trace("PRODUCER: numItems: %d\n", numItems);
 
-    dbg_info("PRODUCER: Acquired Lock!\n");
-    while(numItems == buffer->capacity){
-      pthread_cond_wait(&full, &lock);
-    }
-    dbg_trace("PRODUCER: Writing to buffer...%s\n", array[itemIndex]);
-    memset(buffer->items[buffer->in], 0, BUFFERSIZE*sizeof(char));
-    strncpy(buffer->items[buffer->in], array[itemIndex], BUFFERSIZE);
-    buffer->in = (buffer->in + 1) % buffer->capacity;
-    ++numItems;
-    dbg_trace("PRODUCER: numItems: %d\n", numItems);
-
-    if(numItems == 1)
-      pthread_cond_signal(&empty);
-    dbg_info("PRODUCER: Releasing Lock!\n");
-
+      if(numItems == 1)
+        pthread_cond_signal(&empty);
+      dbg_info("PRODUCER: Releasing Lock!\n");
     pthread_mutex_unlock(&lock);
-    // End
+    // Exit: Critical Section
   }
+  // Ask the consumer to finish
+  pthread_mutex_lock(&lock);
+    dbg_info("PRODUCER: Asked the consumer to finish!\n");
+    producerDone = true;
+    pthread_cond_signal(&empty);
+  pthread_mutex_unlock(&lock);
+
+  dbg_info("PRODUCER: EXIT!\n");
   pthread_exit(NULL);
 }
 
+
+
+/* Consumer */
 static void *consumerThread(void *arg)
 {
   // Get the handle for the data
@@ -72,29 +84,30 @@ static void *consumerThread(void *arg)
 
   while(1)
   {
-    // Critical Section
+    // Enter: Critical Section
     pthread_mutex_lock(&lock);
+      dbg_info("CONSUMER: Acquired Lock!\n");
+      while(numItems == 0 && producerDone == false ){
+        pthread_cond_wait(&empty, &lock);
+      }
+      if(numItems == 0 && producerDone == true){
+        pthread_mutex_unlock(&lock);
+        break;
+      }
+      dbg_info("CONSUMER: Reading from buffer...\n");
+      print_output("%s\n", buffer->items[buffer->out]);
+      buffer->out = (buffer->out + 1) % buffer->capacity;
+      --numItems;
+      dbg_trace("CONSUMER: numItems: %d\n", numItems);
 
-    dbg_info("CONSUMER: Acquired Lock!\n");
-    while(numItems == 0){
-      pthread_cond_wait(&empty, &lock);
-    }
-    dbg_info("CONSUMER: Reading from buffer...\n");
-    print_output("%s\n", buffer->items[buffer->out]);
-    buffer->out = (buffer->out + 1) % buffer->capacity;
-    --numItems;
-    dbg_trace("CONSUMER: numItems: %d\n", numItems);
-
-    if(numItems == buffer->capacity-1)
-      pthread_cond_signal(&full);
-    dbg_info("CONSUMER: Releasing Lock!\n");
-
+      if(numItems == buffer->capacity-1)
+        pthread_cond_signal(&full);
+      dbg_info("CONSUMER: Releasing Lock!\n");
     pthread_mutex_unlock(&lock);
-    // End
-    sleep(rand() % SLEEP_LIMIT);
+    // Exit: Critical Section
   }
-
-    pthread_exit(NULL);
+  dbg_info("CONSUMER: EXIT!\n");
+  pthread_exit(NULL);
 }
 
 
@@ -161,6 +174,7 @@ int main(int argc, char const *argv[])
 
   for(int i=0; i<NUM_THREADS; i++) {
     pthread_join(ThreadID[i], NULL);
+    dbg_trace("MAIN: Thread %d terminated!\n", i);
   }
 
   // Cleanup
